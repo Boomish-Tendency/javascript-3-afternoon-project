93e2e921e779277e372b939a2013c0f550c036c5
Boomish-Tendency <bklindeboom@gmail.com>
2020-10-11 11:56:33 -0400

    destructuring complete

--- destructuring.js
+++ destructuring.js
@@ -113,5 +113,13 @@ function largeNumbers({ first, second, third }) {
 */
 
 //Code Here
-
+function numberGroups({a, b, c}){
+  if (a.length > b.length && a.length > c.length){
+    return a;
+  }
+  else if (b.length > a.length && b.length > c.length){
+    return b;
+  }
+  else return c;
+}
 
